15c94cb01f72bb472c3eaa1192d2e3ff9e6dc2a3
kingle1024 <teran1024@naver.com>
2025-08-26 20:49:34 +0900

    ✨ Feat: Board 명칭 변경, 변수 추가

--- src/main/kotlin/com/connect/service/board/BoardService.kt
+++ src/main/kotlin/com/connect/service/board/BoardService.kt
@@ -6,13 +6,13 @@ import org.springframework.transaction.annotation.Transactional // <- 임포트 
 @Service
 class BoardService(private val boardRepository: BoardRepository) { // 필드 이름도 boardRepository로 변경!
 
-    fun getAllBoards(): List<Board> {
+    fun getAllBoards(): List<BoardMst> {
         return boardRepository.findAll()
     }
 
     @Transactional
-    fun createBoard(content: String): Board {
-        val newBoard = Board(content = content)
-        return boardRepository.save(newBoard)
+    fun createBoard(title: String, content: String, author: String): BoardMst {
+        val newBoardMst = BoardMst(title = title, content = content, author = author)
+        return boardRepository.save(newBoardMst)
     }
 }
