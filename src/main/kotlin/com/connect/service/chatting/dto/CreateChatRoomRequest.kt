0d45cc6016e806ec2cbdbd1f87e4735f726c1239
kingle1024 <teran1024@naver.com>
2025-12-08 20:52:24 +0900

    ✨ Feat:(#75): [친구] 특정 사용자와 1:1 대화 방을 생성하는 API 추가

---
+++ src/main/kotlin/com/connect/service/chatting/dto/CreateChatRoomRequest.kt
@@ -0,0 +1,7 @@
+package com.connect.service.chatting.dto
+
+data class CreateChatRoomRequest(
+    val roomId: String,
+    val userId: String,
+    val roomName: String? = null // roomName은 optional이라서 기본값 null을 줘도 좋아요
+)
